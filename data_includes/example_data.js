23f2f52f9e3a029c8835952b81b0b3110b5f545e
PennController <39499242+PennController@users.noreply.github.com>
2018-08-22 12:34:58 -0400

    Update example_data.js

--- data_includes/example_data.js
+++ data_includes/example_data.js
@@ -1,7 +1,10 @@
 var shuffleSequence = seq(randomize("rating"), randomize("input"));
 PennController.ResetPrefix(null);
 
-PennController.FeedItems( PennController.GetTable("ratings.csv") ,
+// As before, we use FeedItems to define a template
+// But now, we also specify that we want to use a subset of the table:
+// this template only uses the rows where Label is 'rating'
+PennController.FeedItems( PennController.defaultTable.filter("Label","rating") ,
     item => PennController(
         newText("A's line", item.A)
             .print()
@@ -15,7 +18,6 @@ PennController.FeedItems( PennController.GetTable("ratings.csv") ,
             .print()
         ,
         newScale("answer",    "Unnatural", "So-so...", "Natural")
-            .settings.log() // We want to collect data here
             .settings.radio()
             .settings.labels("bottom")
             .settings.center()
@@ -29,7 +31,9 @@ PennController.FeedItems( PennController.GetTable("ratings.csv") ,
     )
 );
 
-PennController.FeedItems( PennController.GetTable("inputs.csv") ,
+// We use FeedItems a second time to define a template for the 'input' trials
+// This template only uses the rows where Label is 'input'
+PennController.FeedItems( PennController.defaultTable.filter("Label","input") ,
     item => PennController(
         newText("warning input", "Please enter some text before validating")
             .settings.bold()
@@ -40,8 +44,8 @@ PennController.FeedItems( PennController.GetTable("inputs.csv") ,
             .settings.italic()
             .print()
         ,
+        // item.Sentence will iteratively take the value of the column 'Sentence' for each row
         newTextInput("alternative")
-            .settings.log() // We want to collect data here
             .settings.before( newText("before", item.Sentence) )
             .print()
         ,
